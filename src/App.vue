a5f7b9ac4b0f6eaccbaef164a15d318bb7e41e8f
Miguel Ángel Monzón Martínez <miguelmonzon.inf@gmail.com>
2024-08-31 20:22:27 -0400

    M6D8 desafio: Updated App.vue to include Bootstrap Icons CSS, and refactored ListCita.vue to display citas in a card layout with conditional styling based on gravedad.

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import CitasMedicas from './components/CitasMedicas.vue'
 </script>
 
 <template>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
   <h1>TodoSalud</h1>
   <CitasMedicas />
 </template>
--- src/components/ListCita.vue
+++ src/components/ListCita.vue
@@ -1,15 +1,30 @@
 <template>
     <div class="container">
         <h2 class="mt-5">Mis Citas Médicas</h2>
-        <ul class="list-group" v-if="listaCitas.length > 0">
-            <li v-for="(cita, idx) in listaCitas" :key="idx" class="list-group-item">
-                {{ cita.paciente }} - {{ cita.fecha }} - {{ cita.horario }} - {{ cita.gravedad }} - {{ cita.motivo }}
-            </li>
-        </ul>
-        <p v-else class="text-danger">Aún no hay citas registradas</p>
+        <div class="row">
+            <div v-if="listaCitas.length > 0" class="col-12">
+                <div v-for="(cita, idx) in listaCitas" :key="idx" class="col-md-4 mb-3">
+                    <div class="card" :class="{
+                        'bg-success bg-opacity-25': cita.gravedad === 1,
+                        'bg-warning bg-opacity-25': cita.gravedad === 2,
+                        'bg-danger bg-opacity-25': cita.gravedad === 3
+                    }">
+                        <div class="card-body">
+                            <h5 class="card-title">Paciente: {{ cita.paciente }}</h5>
+                            <h6 class="card-subtitle mb-2 text-muted">Fecha de Atención: {{ cita.fecha }}</h6>
+                            <p class="card-text">Hora de Atención: {{ cita.horario }}</p>
+                            <p class="card-text">Motivo de Consulta: {{ cita.motivo }}</p>
+                            <button class="btn btn-danger btn-sm">
+                                <i class="bi bi-trash"></i> Eliminar
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <p v-else class="text-danger">Aún no hay citas registradas</p>
+        </div>
     </div>
 </template>
-
 
 <script>
 export default {
@@ -19,4 +34,5 @@ export default {
     },
 };
 </script>
+
 <style scoped></style>
