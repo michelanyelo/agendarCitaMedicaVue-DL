addd679243fc4a737e8c96b1b51d5c6c774c65f0
Miguel Ángel Monzón Martínez <miguelmonzon.inf@gmail.com>
2024-08-31 20:26:30 -0400

    M6D8 desafio: Updated ListCita.vue: changed div class from 'col-12' to 'row', modified card layout and styling

--- src/components/ListCita.vue
+++ src/components/ListCita.vue
@@ -2,7 +2,7 @@
     <div class="container">
         <h2 class="mt-5">Mis Citas Médicas</h2>
         <div class="row">
-            <div v-if="listaCitas.length > 0" class="col-12">
+            <div v-if="listaCitas.length > 0" class="row">
                 <div v-for="(cita, idx) in listaCitas" :key="idx" class="col-md-4 mb-3">
                     <div class="card" :class="{
                         'bg-success bg-opacity-25': cita.gravedad === 1,
@@ -11,10 +11,10 @@
                     }">
                         <div class="card-body">
                             <h5 class="card-title">Paciente: {{ cita.paciente }}</h5>
-                            <h6 class="card-subtitle mb-2 text-muted">Fecha de Atención: {{ cita.fecha }}</h6>
-                            <p class="card-text">Hora de Atención: {{ cita.horario }}</p>
-                            <p class="card-text">Motivo de Consulta: {{ cita.motivo }}</p>
-                            <button class="btn btn-danger btn-sm">
+                            <h6 class="card-subtitle text-muted">Fecha de Atención: {{ cita.fecha }}</h6>
+                            <p class="card-text m-0 p-0">Hora de Atención: {{ cita.horario }}</p>
+                            <p class="card-text m-0 p-0">Motivo de Consulta: {{ cita.motivo }}</p>
+                            <button class="btn btn-danger btn-sm mt-3">
                                 <i class="bi bi-trash"></i> Eliminar
                             </button>
                         </div>
